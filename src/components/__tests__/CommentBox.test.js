2b37681774234f3dea42f6544cf9089c4a4bc76d
ArtKar95 <artkarapetyan1995@gmail.com>
2021-11-20 16:32:29 +0400

    test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { mount } from "enzyme";
-import CommentBox from "../CommentBox";
-import Root from "../../Root";
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from '../CommentBox';
+import Root from '../../Root';
 
 let wrapped;
 
 beforeEach(() => {
   wrapped = mount(
     <Root>
-      <CommentBox />
+       <CommentBox />
     </Root>
   );
 });
@@ -17,26 +17,26 @@ afterEach(() => {
   wrapped.unmount();
 });
 
-it("has a textarea and two buttons", () => {
-  expect(wrapped.find("textarea").length).toEqual(1);
-  expect(wrapped.find("button").length).toEqual(2);
+it('has a textarea and two buttons', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(2);
 });
-describe("the text area", () => {
+describe('the text area', () => {
   beforeEach(() => {
-    wrapped.find("textarea").simulate("change", {
-      target: { value: "new comment" },
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' },
     });
     wrapped.update();
   });
 
-  it("has a text area and user can type in", () => {
-    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  it('has a text area and user can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
   });
 
-  it("after submit text area get empty", () => {
-    wrapped.find("form").simulate("submit");
+  it('after submit text area get empty', () => {
+    wrapped.find('form').simulate('submit');
     wrapped.update();
 
-    expect(wrapped.find("textarea").prop("value")).toEqual("");
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
   });
 });
